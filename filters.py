5b38f697492e3c1a5e87f394c7ab444f88d2e3c7
Riverlance <biel94moraes@msn.com>
2019-12-08 22:16:45 -0300

    Sobel filter fix (abs(slice) + abs(mask))

--- filters.py
+++ filters.py
@@ -283,13 +283,27 @@ def apply_average_filter(original_matrix, mask_path, channels=1):
 
 
 def apply_sobel_filter(original_matrix, mode=None, channels=1):
+    def on_mask_values(slice_matrix, mask_matrix, channels=1):  # Like default_on_mask_values, but with abs on result
+        ret = numpy.zeros(channels)
+
+        for i in range(slice_matrix.shape[0]):
+            for j in range(slice_matrix.shape[1]):
+                slice_r, slice_g, slice_b = slice_matrix[i, j]
+                mask_r, mask_g, mask_b = mask_matrix[i, j]
+                ret = ret[0] + slice_r * mask_r, ret[1] + slice_g * mask_g, ret[2] + slice_b * mask_b
+
+        ret = max(0, min(round(abs(ret[0])), 255)), \
+              max(0, min(round(abs(ret[1])), 255)), \
+              max(0, min(round(abs(ret[2])), 255))
+        return ret
+
     # Apply Sobel vertical border detection
-    vertical = on_mask_pixel(original_matrix, 'masks/sobel_vertical.txt', default_on_mask_values, channels)
+    vertical = on_mask_pixel(original_matrix, 'masks/sobel_vertical.txt', on_mask_values, channels)
     if mode is 1:
         return vertical
 
     # Apply Sobel horizontal border detection
-    horizontal = on_mask_pixel(original_matrix, 'masks/sobel_horizontal.txt', default_on_mask_values, channels)
+    horizontal = on_mask_pixel(original_matrix, 'masks/sobel_horizontal.txt', on_mask_values, channels)
     if mode is 0:
         return horizontal
 
